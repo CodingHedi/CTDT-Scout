7249ad611397785e2c9613e343f2795bd064ef80
Hédi <hedi.soumri+github@gmail.com>
2020-03-05 16:33:17 +0100

    Search + better filters

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -7,12 +7,20 @@
       <option>{{ typeForColor.rouge }}</option>
       <option>{{ typeForColor.vert }}</option>
     </select>
-    &nbsp;&nbsp;&nbsp;<span> Sélectionné : {{ selected }}</span>
+    <span> Sélectionné : {{ selected }}</span>
     <br />
     <br />
   </div>
   <div class="item-container">
-    <div v-for="item in filteredPlayers" v-bind:key="item.id" class="item">
+    <div class="autocomplete">
+      <input type="text" v-model="search" @input="onChange" />
+      <ul v-show="isOpen" class="autocomplete-results">
+        <li v-for="(item) in filteredPlayerslist" v-bind:key="item.id" @click="setResult(item.name)" class="autocomplete-result">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+    <div v-for="item in filteredPlayerslist" v-bind:key="item.id" class="item">
       {{ item.name }}
       <br />
       <img v-bind:src="image(item.desc.img)" />
@@ -31,17 +39,37 @@ const typeForColor = {
   "bleu": "Vit",
   "rouge": "Force",
   "vert": "Tech"
-}
+};
 
 export default {
   name: 'Grid',
   components: {},
   computed: {
-    filteredPlayers() {
-      return this.playerJSON.players.filter(p => this.type(p.desc.color) === this.selected || this.selected === '')
+    filteredPlayerslist() {
+      let currentPlayers = this.playerJSON.players;
+      currentPlayers = this.filteredPlayersByType(currentPlayers)
+      //currentPlayers = this.filteredPlayersByRegion(currentPlayers)
+      //currentPlayers = this.filteredPlayersByRarity(currentPlayers)
+      currentPlayers = this.filteredPlayersByName(currentPlayers)
+      return currentPlayers
     }
   },
   methods: {
+
+    filteredPlayersByName(currentPlayers) {
+      return currentPlayers.filter(item => item.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1)
+    },
+
+    setResult(item) {
+      this.search = item;
+      this.isOpen = false;
+    },
+
+    filteredPlayersByType(currentPlayers) {
+      return currentPlayers.filter(p => this.type(p.desc.color) === this.selected || this.selected === '')
+
+    },
+
     type(color) {
       color = color.toLowerCase()
       if (color in typeForColor) {
@@ -58,13 +86,21 @@ export default {
 
     image(id) {
       return images(`./${id}.gif`)
+    },
+    onChange() {
+      this.isOpen = true;
+      if (this.search === '') {
+        this.isOpen = false;
+      }
     }
   },
   data() {
     return {
       playerJSON: json,
       selected: '',
-      typeForColor: typeForColor
+      search: '',
+      typeForColor: typeForColor,
+      isOpen: false
     }
   }
 }
@@ -93,5 +129,30 @@ export default {
 .reduce {
   width: 18%;
   height: 18%;
+}
+
+.autocomplete {
+  position: relative;
+  width: 130px;
+}
+
+.autocomplete-results {
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  height: 120px;
+  overflow: auto;
+}
+
+.autocomplete-result {
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
+}
+
+.autocomplete-result:hover {
+  background-color: #4AAE9B;
+  color: white;
 }
 </style>
